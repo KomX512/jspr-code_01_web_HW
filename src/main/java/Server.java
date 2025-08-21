276f371d1c6dfddeeff492920b4eb3026c523a45
KomX <komx512@outlook.com>
2025-08-21 11:49:27 +0500

    task ONE done.

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -3,16 +3,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Server {
-
-    public Server(){
-
-    }
 
     private static final int THREADS_COUNT = 64;
     final static List<String> VALID_PATHS = List.of(
@@ -30,71 +30,94 @@ public class Server {
 
     public void start(int port) {
 
+        final ExecutorService executorService = Executors.newFixedThreadPool(THREADS_COUNT);
+        ThreadPoolExecutor = new ThreadPoolExecutor()
         try (final var serverSocket = new ServerSocket(port)) {
             while (true) {
-                try (
-                        final var socket = serverSocket.accept();
-                        final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                        final var out = new BufferedOutputStream(socket.getOutputStream());
-                ) {
-                    // read only request line for simplicity
-                    // must be in form GET /path HTTP/1.1
-                    final var requestLine = in.readLine();
-                    final var parts = requestLine.split(" ");
-
-                    if (parts.length != 3) {
-                        // just close socket
-                        continue;
-                    }
-
-                    final var path = parts[1];
-                    if (!VALID_PATHS.contains(path)) {
-                        out.write((
-                                "HTTP/1.1 404 Not Found\r\n" +
-                                        "Content-Length: 0\r\n" +
-                                        "Connection: close\r\n" +
-                                        "\r\n"
-                        ).getBytes());
-                        out.flush();
-                        continue;
-                    }
-
-                    final var filePath = Path.of(".", "public", path);
-                    final var mimeType = Files.probeContentType(filePath);
-
-                    // special case for classic
-                    if (path.equals("/classic.html")) {
-                        final var template = Files.readString(filePath);
-                        final var content = template.replace(
-                                "{time}",
-                                LocalDateTime.now().toString()
-                        ).getBytes();
-                        out.write((
-                                "HTTP/1.1 200 OK\r\n" +
-                                        "Content-Type: " + mimeType + "\r\n" +
-                                        "Content-Length: " + content.length + "\r\n" +
-                                        "Connection: close\r\n" +
-                                        "\r\n"
-                        ).getBytes());
-                        out.write(content);
-                        out.flush();
-                        continue;
-                    }
-
-                    final var length = Files.size(filePath);
-                    out.write((
-                            "HTTP/1.1 200 OK\r\n" +
-                                    "Content-Type: " + mimeType + "\r\n" +
-                                    "Content-Length: " + length + "\r\n" +
-                                    "Connection: close\r\n" +
-                                    "\r\n"
-                    ).getBytes());
-                    Files.copy(filePath, out);
-                    out.flush();
+                try {
+                    final Socket socket = serverSocket.accept();
+                    executorService.submit(newServerTask(socket));
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private Runnable newServerTask(Socket socket) {
+        return () -> {
+            try {
+                connection(socket);
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
+    }
+
+    private void connection(Socket socket) throws IOException {
+        try (final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             final var out = new BufferedOutputStream(socket.getOutputStream())) {
+            final var requestLine = in.readLine();
+            final var parts = requestLine.split(" ");
+
+            if (parts.length != 3) {
+                // just close socket
+                return;
+            }
+
+            final var path = parts[1];
+            if (!VALID_PATHS.contains(path)) {
+                outNotFound(out);
+                return;
+            }
+
+            sendResponse(out, path);
+        }
+    }
+
+    private static void sendResponse(BufferedOutputStream out, String path) throws IOException {
+        final var filePath = Path.of(".", "public", path);
+        final var mimeType = Files.probeContentType(filePath);
+
+        if (path.equals("/classic.html")) {
+            final var template = Files.readString(filePath);
+            final var content = template.replace(
+                    "{time}",
+                    LocalDateTime.now().toString()
+            ).getBytes();
+            outWrite(out, content.length, mimeType);
+            out.write(content);
+            out.flush();
+            return;
+        }
+
+        final var length = Files.size(filePath);
+        outWrite(out, length, mimeType);
+        Files.copy(filePath, out);
+        out.flush();
+    }
+
+    private static void outWrite(BufferedOutputStream out, long length, String mimeType) throws IOException {
+        out.write((
+                "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: " + mimeType + "\r\n" +
+                        "Content-Length: " + length + "\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+    }
+
+    private static void outNotFound(BufferedOutputStream out) throws IOException {
+        out.write((
+                "HTTP/1.1 404 Not Found\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.flush();
     }
 }
